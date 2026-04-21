92fe29133fea60654b98ea744b31aae3a44f3c37
michalhagos <mhayele3@gmail.com>
2026-04-21 08:43:11 -0500

    Created a Scanner and added an array of 10 quotes. did the try and catch to the statements to avoid errors.

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FamousQuotes {
+ static Scanner thScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String [] quotes = new String[10];
+        quotes[0]= "Everything is temporary";
+        quotes[1]= "Nothing is impossible";
+        quotes[2]= "Believe you can and you are halfway there ";
+        quotes[3]= "Done is better than perfect";
+        quotes[4]= "Work hard stay humble ";
+        quotes[5]= "Dream big. Pray bigger";
+        quotes[6]= "Choose to shine";
+        quotes[7]= "Growth over comfort";
+        quotes[8]= "Stay focused";
+        quotes[9]= "Finish trong";
+
+
+
+
+try{
+    System.out.println("Choose a number 0 to 9 " );
+    int number = thScanner.nextInt();
+    System.out.println(quotes[number]);
+
+}catch (Exception e){
+    System.out.println("try again");
+}
+
+    }
 }
